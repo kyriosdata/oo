16fc7118cb1769bd030517a5c67718dd292532e8
kyriosdata <F@byo*06087691>
2023-10-02 11:26:04 -0300

    Diretório apropriado

--- design/interface/exemplo/src/main/java/com/github/kyriosdata/BananaPorPrecoComparator.java
+++ design/interface/exemplo/src/main/java/com/github/kyriosdata/BananaPorPrecoComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 /**
  * Implementa comparador de bananas por preço.
  */
-class BananaPorPrecoComparator implements Comparator<Banana> {
+public class BananaPorPrecoComparator implements Comparator<Banana> {
     @Override
     public int compare(Banana o1, Banana o2) {
         return Double.compare(o1.getPreco(), o2.getPreco());
